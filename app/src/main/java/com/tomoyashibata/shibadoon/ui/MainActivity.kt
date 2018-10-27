3ca22dd66b895244ffeea70af21cb06ed47a136d
TomoyaShibata <wind.of.hometown@gmail.com>
2018-10-27 13:12:53 +0900

    :+1: Toolbar の管理は MainActivity で行うようにした

--- app/src/main/java/com/tomoyashibata/shibadoon/ui/MainActivity.kt
+++ app/src/main/java/com/tomoyashibata/shibadoon/ui/MainActivity.kt
@@ -4,13 +4,17 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.Navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.tomoyashibata.shibadoon.R
+import kotlinx.android.synthetic.main.main_activity.*
 
 class MainActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     this.setContentView(R.layout.main_activity)
+
+    this.setupToolbar()
   }
 
   override fun onSupportNavigateUp() =
@@ -18,5 +22,10 @@ class MainActivity : AppCompatActivity() {
 
   override fun onNewIntent(intent: Intent?) {
     this.intent.data = intent?.data
+  }
+
+  fun setupToolbar() {
+    this.setSupportActionBar(this.main_toolbar)
+    this.setupActionBarWithNavController(findNavController(this, R.id.my_nav_host_fragment))
   }
 }
