5c77c604cebc5fd369fac005e3273847dbfbbee0
TomoyaShibata <wind.of.hometown@gmail.com>
2018-10-29 08:05:18 +0900

    :bug: ログイン画面が開けない問題を修正
    KOIN による依存注入時に必要な情報が不足していた

--- app/src/main/java/com/tomoyashibata/shibadoon/model/repository/AppRepository.kt
+++ app/src/main/java/com/tomoyashibata/shibadoon/model/repository/AppRepository.kt
@@ -10,7 +10,7 @@ import timber.log.Timber
 
 class AppRepository : KoinComponent {
   suspend fun registerApp(instance: String, app: App): Authentication? {
-    val mastodonApi: MastodonApi by inject { parametersOf(instance, "") }
+    val mastodonApi: MastodonApi by inject("withParams") { parametersOf(instance, "") }
 
     return try {
       mastodonApi.registerApp(app).await()
